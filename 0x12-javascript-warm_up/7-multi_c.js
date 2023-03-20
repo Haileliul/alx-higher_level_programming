6c99240ac2ed7e6150c79c591f06806b110478dd
haile <haile@localhost>
2023-03-20 12:18:23 +0300

    ha

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const n = process.argv[2];
+if (typeof process.argv[2] === 'undefined') {
+    console.log("Missing number of occurrences");
+}
+for (let i = 0; i < n; i++){
+    console.log("C is fun");
+}

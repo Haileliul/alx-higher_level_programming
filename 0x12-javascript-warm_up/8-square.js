9a296a3cdee6685acb56cef19e674869937defc5
haile <haile@localhost>
2023-03-20 13:01:01 +0300

    ha

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,15 @@
 #!/usr/bin/node
 const num = process.argv[2];
-let s = '';
+
 if (isNaN(parseInt(process.argv[2]))) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < num; i++) {
+    let s = '';
     for (let j = 0; j < num; j++) {
       s = s + 'X';
     }
-    s += '\n';
+    // s += '\n';
+    console.log(s);
   }
-  console.log(s);
 }

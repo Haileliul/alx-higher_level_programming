040db75594f73838b54ad69c1cedfbfa853576ca
Haileliul <hailetechlover@gmail.com>
2024-02-13 16:51:41 +0300

    Update 9-add.js
    updated

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
-const first = parseInt(process.argv[2]);
-const second = parseInt(process.argv[3]);
-function add (a, b) {
-  if (isNaN(a) || isNaN(b)) {
-    return NaN;
-  }
-  return a + b;
+const args = process.argv;
+const num1 = parseInt(args[2]);
+const num2 = parseInt(args[3]);
+
+function add(a, b) {
+  console.log(a + b);
 }
-console.log(add(first, second));
+if (isNaN(num1) || isNaN(num2)) {
+  console.log("Note A number");
+} else {
+  add(num1, num2);
+}

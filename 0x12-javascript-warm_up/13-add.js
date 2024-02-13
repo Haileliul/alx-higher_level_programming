98769be5180d42383273f1af67f67399be538e6a
Haileliul <hailetechlover@gmail.com>
2024-02-13 16:41:21 +0300

    Update 13-add.js
    updated

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 const args = process.argv;
+const num1 = parseInt(args[2]);
+const num2 = parseInt(args[3]);
 function add(a,b) {
   console.log(a + b);
 }
-add(args[2], args[3] );
+add(num1, num2 );

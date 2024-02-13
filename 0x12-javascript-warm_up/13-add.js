98685c4711b51a73679811642b8ec5f25bd65812
Haileliul <hailetechlover@gmail.com>
2024-02-13 16:36:55 +0300

    Update 13-add.js
    update has been made to the change

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-module.exports = {
-  add: function (a, b) {
-    return a + b;
-  }
-};
+const args = process.argv;
+const  num1 = args[2];
+const num2= args[3];
+
+function add(a,b) {
+  console.log(a + b);
+}
+add(num1,num2);
